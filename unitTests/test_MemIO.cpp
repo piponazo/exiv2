1e39d96d60eff6db7921091cad0f1716d71df158
Luis Diaz Mas <piponazo@gmail.com>
2018-11-27 22:59:39 +0100

    Fix warnings in MemIo unit tests

--- unitTests/test_MemIO.cpp
+++ unitTests/test_MemIO.cpp
@@ -12,7 +12,7 @@ struct ADefaultMemIO: public testing::Test
 
 TEST_F(ADefaultMemIO, initialValues)
 {
-    ASSERT_EQ(0, mem.size());
+    ASSERT_EQ(0ul, mem.size());
     ASSERT_EQ(0, mem.tell());
     ASSERT_TRUE(mem.isopen());
     ASSERT_FALSE(mem.error());
@@ -29,13 +29,13 @@ TEST_F(ADefaultMemIO, writeAllocatesAutomaticallyMemory)
 {
     byte x[] = {1,2,3};
     ASSERT_EQ(3, mem.write(x, 3));
-    ASSERT_EQ(3, mem.size());
+    ASSERT_EQ(3ul, mem.size());
     ASSERT_EQ(3, mem.tell());
 }
 
 TEST_F(ADefaultMemIO, writeWithNullPointerDoesNotThrow)
 {
     ASSERT_NO_THROW(mem.write(NULL, 3));
-    ASSERT_EQ(3, mem.size());
+    ASSERT_EQ(3ul, mem.size());
     ASSERT_EQ(3, mem.tell());
 }
