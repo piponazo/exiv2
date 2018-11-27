e55cbab25f2c4cd151a6e691151302f7d499d132
Luis Díaz Más <piponazo@gmail.com>
2018-11-27 18:38:12 +0100

    Few characterization tests for MemIo

---
+++ unitTests/test_MemIO.cpp
@@ -0,0 +1,41 @@
+#include "MemIo.hpp"
+#include "error.hpp"
+
+#include "gtestwrapper.h"
+
+using namespace Exiv2;
+
+struct ADefaultMemIO: public testing::Test
+{
+    MemIo mem;
+};
+
+TEST_F(ADefaultMemIO, initialValues)
+{
+    ASSERT_EQ(0, mem.size());
+    ASSERT_EQ(0, mem.tell());
+    ASSERT_TRUE(mem.isopen());
+    ASSERT_FALSE(mem.error());
+    ASSERT_FALSE(mem.eof());
+    ASSERT_STREQ("MemIo", mem.path().c_str());
+}
+
+TEST_F(ADefaultMemIO, closeAlwaysReturn0)
+{
+    ASSERT_EQ(0, mem.close());
+}
+
+TEST_F(ADefaultMemIO, writeAllocatesAutomaticallyMemory)
+{
+    byte x[] = {1,2,3};
+    ASSERT_EQ(3, mem.write(x, 3));
+    ASSERT_EQ(3, mem.size());
+    ASSERT_EQ(3, mem.tell());
+}
+
+TEST_F(ADefaultMemIO, writeWithNullPointerDoesNotThrow)
+{
+    ASSERT_NO_THROW(mem.write(NULL, 3));
+    ASSERT_EQ(3, mem.size());
+    ASSERT_EQ(3, mem.tell());
+}
