f63e653ea590d3d809f70fa16202834fa6266ccf
Luis Diaz Mas <piponazo@gmail.com>
2018-11-28 18:49:06 +0100

    It should fix the PsdImage tests on OSX

--- unitTests/test_PsdImage.cpp
+++ unitTests/test_PsdImage.cpp
@@ -2,15 +2,14 @@
 
 #include "gtestwrapper.h"
 
-using namespace Exiv2;
-
 struct AFakePsdImage : public testing::Test
 {
     AFakePsdImage() : image(ptr)
     {
     }
-    BasicIo::AutoPtr ptr;
-    PsdImage image;
+
+    Exiv2::BasicIo::AutoPtr ptr;
+    Exiv2::PsdImage image;
 };
 
 TEST_F(AFakePsdImage, isConstructed)
@@ -19,27 +18,27 @@ TEST_F(AFakePsdImage, isConstructed)
 
 TEST_F(AFakePsdImage, setCommentThrows)
 {
-    EXPECT_THROW(image.setComment(("blabla")), Exiv2::Error);
+    EXPECT_THROW(image.setComment(("blabla")), Exiv2::AnyError);
 }
 
 TEST_F(AFakePsdImage, throwsTryingToReadMetadata)
 {
-    EXPECT_THROW(image.readMetadata(), Exiv2::Error);
+    EXPECT_THROW(image.readMetadata(), Exiv2::AnyError);
 }
 
 TEST_F(AFakePsdImage, throwsTryingToWriteMetadata)
 {
-    EXPECT_THROW(image.writeMetadata(), Exiv2::Error);
+    EXPECT_THROW(image.writeMetadata(), Exiv2::AnyError);
 }
 
 const std::string psdPath = TEST_DATA_DIR + std::string("exiv2-photoshop.psd");
 
 struct APsdImage : public testing::Test
 {
-    APsdImage() : image(ImageFactory::open(psdPath, false))
+    APsdImage() : image(Exiv2::ImageFactory::open(psdPath, false))
     {
     }
-    Image::AutoPtr image;
+    Exiv2::Image::AutoPtr image;
 };
 
 TEST_F(APsdImage, throwsTryingToReadMetadata)
