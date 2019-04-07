500f55792bea797f5453c1f36663889fb66de0e2
Luis Díaz Más <piponazo@gmail.com>
2019-04-07 22:56:09 +0200

    Use auto and give format to some code related with regex

--- src/actions.cpp
+++ src/actions.cpp
@@ -537,52 +537,55 @@ namespace Action {
         bool noExif = false;
         if (Params::instance().printTags_ & Exiv2::mdExif) {
             const Exiv2::ExifData& exifData = image->exifData();
-            for (Exiv2::ExifData::const_iterator md = exifData.begin();
-                 md != exifData.end(); ++md) {
+            for (auto md = exifData.begin(); md != exifData.end(); ++md) {
                 ret |= printMetadatum(*md, image);
             }
-            if (exifData.empty()) noExif = true;
+            if (exifData.empty())
+                noExif = true;
         }
 
         bool noIptc = false;
         if (Params::instance().printTags_ & Exiv2::mdIptc) {
             const Exiv2::IptcData& iptcData = image->iptcData();
-            for (Exiv2::IptcData::const_iterator md = iptcData.begin();
-                 md != iptcData.end(); ++md) {
+            for (auto md = iptcData.begin(); md != iptcData.end(); ++md) {
                 ret |= printMetadatum(*md, image);
             }
-            if (iptcData.empty()) noIptc = true;
+            if (iptcData.empty())
+                noIptc = true;
         }
 
         bool noXmp = false;
         if (Params::instance().printTags_ & Exiv2::mdXmp) {
             const Exiv2::XmpData& xmpData = image->xmpData();
-            for (Exiv2::XmpData::const_iterator md = xmpData.begin();
-                 md != xmpData.end(); ++md) {
+            for (auto md = xmpData.begin(); md != xmpData.end(); ++md) {
                 ret |= printMetadatum(*md, image);
             }
-            if (xmpData.empty()) noXmp = true;
+            if (xmpData.empty())
+                noXmp = true;
         }
 
         // With -v, inform about the absence of any (requested) type of metadata
         if (Params::instance().verbose_) {
-            if (noExif) std::cerr << path_ << ": " << _("No Exif data found in the file\n");
-            if (noIptc) std::cerr << path_ << ": " << _("No IPTC data found in the file\n");
-            if (noXmp)  std::cerr << path_ << ": " << _("No XMP data found in the file\n");
+            if (noExif)
+                std::cerr << path_ << ": " << _("No Exif data found in the file\n");
+            if (noIptc)
+                std::cerr << path_ << ": " << _("No IPTC data found in the file\n");
+            if (noXmp)
+                std::cerr << path_ << ": " << _("No XMP data found in the file\n");
         }
 
         // With -g or -K, return -3 if no matching tags were found
         int rc = 0;
-        if ((!Params::instance().greps_.empty() || !Params::instance().keys_.empty()) && !ret) rc = 1;
+        if ((!Params::instance().greps_.empty() || !Params::instance().keys_.empty()) && !ret)
+            rc = 1;
 
         return rc;
-    } // Print::printMetadata
+    }
 
     bool Print::grepTag(const std::string& key)
     {
         bool result=Params::instance().greps_.empty();
-        for (Params::Greps::const_iterator g = Params::instance().greps_.begin();
-                !result && g != Params::instance().greps_.end(); ++g)
+        for (auto g = Params::instance().greps_.begin(); !result && g != Params::instance().greps_.end(); ++g)
         {
 #if defined(EXV_HAVE_REGEX_H)
             result = regexec( &(*g), key.c_str(), 0, nullptr, 0) == 0 ;
